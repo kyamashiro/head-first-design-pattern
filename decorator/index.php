9646c3d78afb91b12de4ee3cae5bf1039e476679
kyamashiro <kyamashiro73@gmail.com>
2019-11-18 08:31:37 +0900

    comment update

--- decorator/index.php
+++ decorator/index.php
@@ -8,10 +8,14 @@ use Decorator\Whip;
 require './vendor/autoload.php';
 
 $espresso = new Espresso();
+// エスプレッソ $ 1.99
 print_r("{$espresso->getDescription()} $ {$espresso->cost()}\n");
 
-$house_blend = new Mocha(new HouseBlend());
-print_r("{$house_blend->getDescription()} $ {$house_blend->cost()}\n");
+// エスプレッソをインスタンス化し、それをトッピングでラップする
+$house_blend_mocha = new Mocha(new HouseBlend());
+// ハウスブレンドコーヒー, モカ $ 1.09
+print_r("{$house_blend_mocha->getDescription()} $ {$house_blend_mocha->cost()}\n");
 
-$house_blend_whip = new Whip(new Mocha(new HouseBlend()));
-print_r("{$house_blend_whip->getDescription()} $ {$house_blend_whip->cost()}\n");
+$house_blend_mocha_whip = new Whip(new Mocha(new HouseBlend()));
+// ハウスブレンドコーヒー, モカ, ホイップ $ 1.59
+print_r("{$house_blend_mocha_whip->getDescription()} $ {$house_blend_mocha_whip->cost()}\n");
